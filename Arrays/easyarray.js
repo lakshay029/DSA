53d7500d033bb8a463419146bf5e87c77ae4f935
Lakshay Choudhary <lakshaychoudhary79@gmail.com>
2026-05-31 21:38:02 +0530

    main

--- Arrays/easyarray.js
+++ Arrays/easyarray.js
@@ -141,23 +141,47 @@ console.log(SortedArray([1,5,3]))
 // Input: [1, 2, 2, 3, 4, 4]
 // Output: [1, 2, 3, 4]
 
+function DuplicateRemove(arr) {
+    let unique = [];
 
-
-
-
-
-
-
-
-
-
+    for (let i = 0; i < arr.length; i++) {
+        if (!unique.includes(arr[i])) {
+            unique.push(arr[i]);
+        }
+    }
+    return unique;
+}
+console.log(DuplicateRemove([1,2,2,3,4,4]));
 
 // 9. Move All Zeros to End
-
 // Move all zeros to the end while maintaining order.
-
 // Input: [0, 1, 0, 3, 12]
 // Output: [1, 3, 12, 0, 0]
+
+function MoveZero(arr){
+    let index=0;
+    for(let i =0; i<arr.length;i++){
+        if(arr[i]!==0){
+            arr[index]=arr[i];
+            index++;
+        }
+    }
+    while(index<arr.length){
+        arr[index]=0;
+        index++;
+    }
+    return arr;
+}
+console.log(MoveZero([1,0,2,3,4]))
+
+
+
+
+
+
+
+
+
 // 10. Linear Search
 
 // Find the index of a target element.
